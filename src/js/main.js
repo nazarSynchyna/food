8250b9959eb26f9d02cb299b5d0b33b2bfd00393
Nazar Synchyna <nsynchyna@gmail.com>
2026-07-27 15:55:47 +0300

    refactor: extract deleteNotDigits helper function

--- src/js/main.js
+++ src/js/main.js
@@ -361,10 +361,14 @@ window.addEventListener("DOMContentLoaded", () => {
     dotArr.push(dot);
   }
 
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
+
   next.addEventListener("click", () => {
-    if (offset == +width.replace(/\D/g, '') * (slides.length - 1))
+    if (offset == deleteNotDigits(width) * (slides.length - 1))
       offset = 0;
-    else offset += +width.replace(/\D/g, "");
+    else offset += deleteNotDigits(width);
 
     slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -380,8 +384,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
   prev.addEventListener("click", () => {
     if (offset === 0)
-      offset = +width.replace(/\D/g, "") * (slides.length - 1);
-    else offset -= +width.replace(/\D/g, "");
+      offset = deleteNotDigits(width) * (slides.length - 1);
+    else offset -= deleteNotDigits(width);
 
     slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -400,7 +404,7 @@ window.addEventListener("DOMContentLoaded", () => {
       const slideTo = e.target.getAttribute("data-slide-to");
 
       slideIndex = slideTo;
-      offset = +width.replace(/\D/g, "") * (slideTo - 1);
+      offset = deleteNotDigits(width) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
